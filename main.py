38f1a821b2e5517cbd3804f846e3f6d48970d231
Arseny Antonov <un57a6l3@gmail.com>
2022-10-10 16:33:09 +0300

    Основа (init, fill, print), сложение

---
+++ main.py
@@ -0,0 +1,69 @@
+class Matrix:
+    # Инициализация матрицы из вложенного списка
+    def __init__(self, data):
+        self.data = data
+        self.mdim = len(data)
+        self.ndim = len(data[0])
+
+    # Создание матрицы, заполненной заданным числом
+    @staticmethod
+    def fill(mdim, ndim, value=1):
+        return Matrix([[value] * ndim for _ in range(mdim)])
+
+    # Вывод матрицы
+    def print(self):
+        for num in self.data:
+            print('\t'.join(map(str, num)))
+
+    # Метод сложения двух матриц
+    @staticmethod
+    def add(op1: 'Matrix', op2: 'Matrix'):
+        # Проверка, совпадают ли размеры матриц
+        if op1.ndim != op2.ndim or op1.mdim != op2.mdim:
+            return type('obj', (object,), {'data': 'IndexError'})
+
+        # Сложение матриц
+        res = Matrix.fill(op1.mdim, op1.ndim)
+        for m in range(res.mdim):
+            for n in range(res.ndim):
+                res.data[m][n] = op1.data[m][n] + op2.data[m][n]
+        return res
+
+    # Метод вычитания двух матриц
+    # TODO
+    def sub():
+        pass
+
+    # Метод умножения двух матриц (или матрицы и числа)
+    # TODO
+    def mul():
+        pass
+
+    # Метод возведения матрицы в степень
+    # TODO
+    def pow():
+        pass
+
+    # Единичная матрица заданного размера
+    # TODO
+    def identity():
+        pass
+
+    # Метод транспонирования матрицы
+    # TODO
+    def transpose():
+        pass
+
+    # Метод для пользовательского ввода
+    # TODO
+    def enter():
+        pass
+
+
+# TODO
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,41 @@
+import unittest
+from main import Matrix
+
+
+class TestMatrix(unittest.TestCase):
+    def testAdd(self):
+        m1 = Matrix.fill(2, 3, 1)
+        m2 = Matrix.fill(2, 2, -1)
+        m3 = Matrix.fill(2, 2, 1)
+        m4 = Matrix([[2, 4, 3], [5, 1, 2]])
+        m5 = Matrix.identity(3)
+        m6 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        m7 = Matrix([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+        m8 = Matrix.fill(3, 3, 1)
+        self.assertEqual(Matrix.add(m1, m1).data, [[2, 2, 2], [2, 2, 2]])
+        self.assertEqual(Matrix.add(m2, m3).data, [[0, 0], [0, 0]])
+        self.assertEqual(Matrix.add(m4, m5).data, "IndexError")
+        self.assertEqual(Matrix.add(m6, m7).data, [[10, 10, 10], [10, 10, 10],
+                                                   [10, 10, 10]])
+        self.assertEqual(Matrix.add(m7, m8).data, [[10, 9, 8], [7, 6, 5],
+                                                   [4, 3, 2]])
+
+    # TODO
+    def testSub(self):
+        pass
+
+    # TODO
+    def testMul(self):
+        pass
+
+    # TODO
+    def testPow(self):
+        pass
+
+    # TODO
+    def testTranspose(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
